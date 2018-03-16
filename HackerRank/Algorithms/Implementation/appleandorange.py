3f66416496d7eb546215fbbc095f7048f40608a4
VanessaKang <vanessakang95@gmail.com>
2018-03-16 00:38:31 -0400

    Completed Hackerrank Challenge: Apple and Orange

--- HackerRank/Algorithms/Implementation/appleandorange.py
+++ HackerRank/Algorithms/Implementation/appleandorange.py
@@ -7,27 +7,18 @@
 # Purpose: Determine how many apples and oranges will fall on Sam's house (i.e., in the inclusive range [s..t]) and print the number of apples that fall in first line of output then print number of oranges as second output.
 
 import sys
+import itertools
 
 
 def appleAndOrange(s, t, a, b, apple, orange):
-
-    # go through apple distances
-    #     do subtraction or addition with position of a
-    #     check if that falls within s through t
-    #         if true, count += 1
-
-    # go through orange distance
-    #     do subtraction or addition with position of b
-    #     check if that result falls within s throught t
-    #         if true, count +=1
-
-    # return counta, count
+    # Initialize counts for Number of Apples/Oranges on the House
     aCount, oCount = 0, 0
 
+    # Calculate the position of the Apple and see if its on the House
     for aDist in apple:
         if a + aDist in range(s, t + 1):
             aCount += 1
-
+    # Calculate the position of the Orange and see if its on the House
     for oDist in orange:
         if b + oDist in range(s, t + 1):
             oCount += 1
@@ -36,13 +27,6 @@ def appleAndOrange(s, t, a, b, apple, orange):
 
 
 if __name__ == "__main__":
-    # s, t = input().strip().split(' ')
-    # s, t = [int(s), int(t)]
-    # s, t = map(int, input().strip().split(' '))
-    # a, b = input().strip().split(' ')
-    # a, b = [int(a), int(b)]
-    # m, n = input().strip().split(' ')
-    # m, n = [int(m), int(n)]
 
     s, t = map(int, input().strip().split(' '))
     a, b = map(int, input().strip().split(' '))
